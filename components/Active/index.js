1420b19af5213ef3c50e1b301e9d244ea1468496
PEIN <cartonweber0426@gmail.com>
2021-10-01 02:53:18 -0700

    change name

--- components/Active/index.js
+++ components/Active/index.js
@@ -6,7 +6,7 @@ const Active = ({ children, state, setState }) => {
         <>
             <div className="relative flex items-center ml-9 activepage" style={{ borderRadius: "36px", backgroundColor: "#D5D3D3", height: "56px" }}>
                 <button className="text-3xl font-bold px-4 z-10" onClick={() => { setState(1) }}>Active</button>
-                <button className="text-3xl font-bold px-1 z-10" onClick={() => { setState(0) }}>InActive</button>
+                <button className="text-3xl font-bold px-1 z-10" onClick={() => { setState(0) }}>Inactive</button>
                 <div className={"absolute z-0 transition-all "} style={{ borderRadius: "44px", backgroundColor: "#5BC2C9", width: "50%", height: "100%", left: state === 1 ? "0" : "50%" }}></div>
             </div>
         </>
